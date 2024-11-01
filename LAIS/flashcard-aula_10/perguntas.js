dbb8e0f73a4b441c279b531fa0409335f68c9d4e
LAYSMONTEIROL <161846793+LAYSMONTEIROL@users.noreply.github.com>
2024-11-01 10:48:53 -0300

    Add files via upload

---
+++ LAIS/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é Liquens?',
+    'O Liquens sao organismos formados por associaçao interespecifica'
+)
+
+criaCartao(
+    'ProgramaçÃo',
+    'Para que serve finalize?',
+    'Pode utilizar o finalize para limpar recursos'
+)
+
+criaCartao(
+    'DESENHO',
+    'Como e o nome das meninas super poderosas?',
+    'Docinho,Lindinha e Florzinha'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bonita em ingles?',
+    'Bonita em ingles e BEAUTIFUL'
+)
